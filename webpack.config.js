3590f5aa98e80f2edc3011e56d19398260d8523e
MdialloC19 <mdiallo3211@gmail.com>
2023-12-08 16:18:56 +0000

    on production

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HTMLWebpackPlugin=require('html-webpack-plugin');
 const MiniCssExtractPlugin=require('mini-css-extract-plugin');
 
 module.exports= {
-    mode: 'development',
+    mode: 'production',
     entry: './src/app.js',
     output:{
        path: path.resolve(__dirname,'dist'),
